96ef52a7868abec2bfec9be371f4e98ce6f25f19
jerikothomas <jerikothomas@gmail.com>
2019-03-15 17:24:02 +0700

    Adding Mongoose

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const path=require('path');
-const expressEdge=require('express-edge');
+const path= require('path');
+const expressEdge =require('express-edge');
 const express= require('express');
+const konekmongo= require('mongoose');
 const aplikasi=new express();
 
+konekmongo.connect('mongodb://localhost:27017/classesauto')
+    .then(()=>'Konek')
+    .catch(err=>console.error('Ada Kesalaahan'));
 aplikasi.use(express.static('public'));
 aplikasi.use(expressEdge);
 aplikasi.set('views',__dirname+'/views');
